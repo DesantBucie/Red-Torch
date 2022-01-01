18b4751f6b0082def8be7e989445368323f2ac55
DesantBucie <mattrusz@gmail.com>
2022-01-01 22:43:16 +0100

    Red Torch innital commit.

---
+++ README.md
@@ -0,0 +1,18 @@
+# RED Torch
+
+Red Torch is a very very (very) simple iOS app that allows you to have a red torch
+using your screen. The App is based on storyboard, so there is wider device range that can run it.
+
+## Running
+
+You can run it if you have an IPhone and a mac.
+
+1. Plug iPhone to your mac.
+2. Select your device in the middle top in the XCode.
+3. Press run it.
+4. App should be installed, but you will need to change settings in order to run it.
+
+    Go to Settings -> General -> VPN and device managment?(or something like that) -> Apple Development -> Red Torch(Click)
+5. Enjoy it, altough it's not too bright is it?
+
+If you see an issue, tell it.
--- Red Torch/ViewController.swift
+++ Red Torch/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var on_button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        on_button.tintColor = UIColor.red;
     }
 
-
+    @IBAction func onClick(_ sender: UIButton) {
+        let currentText = sender.titleLabel?.text
+        if(currentText == "On"){
+            on_button.setTitle("Off", for:.normal);
+            self.view.backgroundColor = UIColor.black;
+            on_button.tintColor = UIColor.red;
+        }
+        else {
+            on_button.setTitle("On", for: .normal);
+            self.view.backgroundColor = UIColor.red;
+            on_button.tintColor = UIColor.black;
+        }
+    }
+    
 }
 
